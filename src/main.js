aabe1b46311565af2ba36f764b1fd69d150bd4aa
zhuanxuhit <wangchao25@baidu.com>
2018-03-18 13:24:15 +0800

    增加导航栏和路由

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import fastclick from 'fastclick'
-// import router from './router'
+import router from './router'
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
@@ -11,6 +11,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   render: h => h(App)
 })
